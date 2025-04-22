4007ea950aed23ea0838afcefb02cf5496ed2e40
heetaru <stas.shkira@gmail.com>
2025-04-23 00:28:22 +0300

    потужний коміт (add country filter)

--- application/universities/filters.py
+++ application/universities/filters.py
@@ -4,10 +4,15 @@ from .models import Faculty, Degree
 
 class FacultyFilter(django_filters.FilterSet):
     degree_type = django_filters.MultipleChoiceFilter(field_name="degrees__type",
-                                                      label="Тип освіти",
+                                                      label="Ступінь освіти",
                                                       choices=Degree.TYPE_CHOICES,
                                                       widget=forms.CheckboxSelectMultiple,)
+    faculty_country = django_filters.MultipleChoiceFilter(field_name="country",
+                                                      label="Країна",
+                                                          choices=[(c["country"], c["country"]) for c in
+                                                                   Faculty.objects.values("country").distinct()],
+                                                          widget=forms.CheckboxSelectMultiple, )
 
     class Meta:
         model = Faculty
-        fields = []
+        fields = ["degree_type", "faculty_country"]
