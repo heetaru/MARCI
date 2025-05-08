e1ebc5d47df29bb4053754ea585fca03fdd0fc93
heetaru <stas.shkira@gmail.com>
2025-05-08 11:33:42 +0300

    add password checker and some backend

--- application/accounts/models.py
+++ application/accounts/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    user_name = models.TextField("Імʼя користувача")
+    user_mail = models.TextField("Пошта користувача")
+    user_password = models.TextField("Пароль користувача")
