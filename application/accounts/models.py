86940322506fb2f26141787fe0bcc27e217476a1
heetaru <stas.shkira@gmail.com>
2025-05-09 22:26:55 +0300

    fix bugs, add a working registration form

--- application/accounts/models.py
+++ application/accounts/models.py
@@ -2,7 +2,13 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
-class Users(models.Model):
+class Students(models.Model):
     user_name = models.TextField("Імʼя користувача")
     user_mail = models.TextField("Пошта користувача")
     user_password = models.TextField("Пароль користувача")
+
+    def __str__(self):
+        return self.user_name
+
+    class Meta:
+        verbose_name = 'Імʼя корситувача'
--- application/accounts/views.py
+++ application/accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from .models import Users
+from .models import Students
 from django.shortcuts import render, redirect
 from django.views.generic import DetailView
 from django.http import JsonResponse
@@ -14,30 +14,13 @@ from django.template.loader import render_to_string
 def register_view(request):
     if request.method == 'POST':
         data = request.POST.dict()
-        users = Users.objects.create(user_name = data['user_name'],
+        users = Students.objects.create(user_name = data['user_name'],
                                      user_mail = data['user_mail'],
                                      user_password = data['user_password']
                                      )
-
         #request.session['faculty_id'] = faculty.id
-        return redirect("faculty-registration-degree")
-    return render(request, 'universities/faculty_registration.html', context)
-    return render(request, 'accounts/registration.html')
+        #return redirect("faculty-registration-degree")
+    return render(request, 'accounts/registration.html', {})
 
 def login_view(request):
     return render(request, 'accounts/login.html')
-
-def faculty_registration(request):
-    context = {}
-    if request.method == 'POST':
-        data = request.POST.dict()
-        faculty = Faculty.objects.create(university_name=data['university_name'],
-                                         country=data['country'],
-                                         city=data['city'],
-                                         faculty_name=data['faculty_name'],
-                                         mail=data['mail'],
-                                         university_description=data['university_description'],
-                                         )
-        request.session['faculty_id'] = faculty.id
-        return redirect("faculty-registration-degree")
-    return render(request, 'universities/faculty_registration.html', context)
