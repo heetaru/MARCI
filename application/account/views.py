93ddc64811f6eadac798e27d57d61210b3f656e6
heetaru <stas.shkira@gmail.com>
2025-05-14 23:49:58 +0300

    add display of faculties that you would like

--- application/account/views.py
+++ application/account/views.py
@@ -26,25 +26,32 @@ def login_view(request):
         user_mail_login = data['user_mail_login']
         user_password_login = data['user_password_login']
         student = Students.objects.filter(user_mail=user_mail_login).first()
-
         if student and check_password(user_password_login, student.user_password):
             request.session['student_id'] = student.id
             request.session['student_name'] = student.user_name
             return redirect('account')
 
-    return render(request, 'account/login.html')
+    return render(request, 'account/login.html' )
 
 
 def account_view(request):
     faculties = Faculty.objects.all()
     user_id = request.session.get('student_id')
-    user = Students.objects.get(id=user_id)
-    saved_faculty_ids = list(SavedFaculty.objects.filter(student=user).values_list('faculty_id', flat=True))
+    student = Students.objects.get(id=user_id)
+    saved_faculty_ids = list(SavedFaculty.objects.filter(student=student).values_list('faculty_id', flat=True))
+    liked_faculties = []
+    for like_id in saved_faculty_ids:
+        if like_id not in liked_faculties:
+            liked_faculties.append(Faculty.objects.get(id=like_id))
+
+
     if 'student_id' not in request.session:
         return redirect('login')
     return render(request, 'account/account.html', {
         'faculties': faculties,
         'saved_faculty_ids': saved_faculty_ids,
+        'student': student,
+        'liked_faculties': liked_faculties,
     })
 
 
